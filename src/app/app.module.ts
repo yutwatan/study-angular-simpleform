3d2db96641a851af7d8820ad9a5bf2de77c1b8d2
yutwatan <yutwatan@yahoo-corp.jp>
2018-06-10 16:06:32 +0900

    create template-driven form into simple-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SimpleFormComponent } from './simple-form/simple-form.component';
@@ -10,7 +11,8 @@ import { SimpleFormComponent } from './simple-form/simple-form.component';
     SimpleFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simple-form.component.css']
 })
 export class SimpleFormComponent implements OnInit {
+  result = '足し算しましょう';
+  text1: string;
+  text2: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  addAndShow(): void {
+    let forResult = '正しい値を入力してください';
+    let int1: number;
+    let int2: number;
+
+    int1 = Number(this.text1);
+    int2 = Number(this.text2);
+
+    if (!Number.isNaN(int1) && !Number.isNaN(int2)) {
+      forResult = `${int1} + ${int2} = ${int1 + int2}`;
+    }
+    this.result = forResult;
+  }
 }
