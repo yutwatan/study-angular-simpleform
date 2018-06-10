16cfd25da8c7e84582ea366ec41cf9b97390dd94
yutwatan <yutwatan@yahoo-corp.jp>
2018-06-10 18:47:14 +0900

    create reactive form into 'better-form'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SimpleFormComponent } from './simple-form/simple-form.component';
@@ -14,7 +14,8 @@ import { BetterFormComponent } from './better-form/better-form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/better-form/better-form.component.ts
+++ src/app/better-form/better-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-better-form',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./better-form.component.css']
 })
 export class BetterFormComponent implements OnInit {
+  calcForm: FormGroup;
+  result = '足し算しましょう';
 
   constructor() { }
 
   ngOnInit() {
+    this.calcForm = new FormGroup({
+      'fieldOne': new FormControl('0', Validators.required),
+      'fieldTwo': new FormControl('0', [Validators.required, Validators.maxLength(5)])
+    });
   }
 
+  get fieldOne() {
+    return this.calcForm.get('fieldOne');
+  }
+  get fieldTwo() {
+    return this.calcForm.get('fieldTwo');
+  }
+
+  addAnyway() {
+    const text1: string = this.fieldOne.value;
+    const text2: string = this.fieldTwo.value;
+    let resultStr = '';
+
+    if (Number.isNaN(Number(text1)) || Number.isNaN(Number(text2))) {
+      resultStr = text1 + text2;
+    } else {
+      resultStr = `${text1} + ${text2} = ${Number(text1) + Number(text2)}`
+    }
+    this.result = resultStr;
+  }
+
+  clearResult() {
+    this.result = '';
+  }
 }
